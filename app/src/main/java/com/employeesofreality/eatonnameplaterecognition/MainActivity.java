fab8b66b666f51a6971127870bbaeede40425dfa
James Finefrock <finefrockjames@gmail.com>
2017-10-21 14:04:08 -0400

    Added implementation to use different fragment for section 1.

--- app/src/main/java/com/employeesofreality/eatonnameplaterecognition/MainActivity.java
+++ app/src/main/java/com/employeesofreality/eatonnameplaterecognition/MainActivity.java
@@ -344,9 +344,13 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            View rootView = inflater.inflate(R.layout.camera_preview, container, false);
+
+            if(getArguments().getInt(ARG_SECTION_NUMBER) != 1) {
+                rootView = inflater.inflate(R.layout.fragment_main, container, false);
+                TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+                textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            }
             return rootView;
         }
     }
